76e5032c5d458cbbd5dcfe19466c4b31ca9dd951
Doffy420 <107606763+Doffy420@users.noreply.github.com>
2022-06-20 20:54:36 +0800

    Add files via upload

---
+++ qtile-rice.sh
@@ -0,0 +1,3 @@
+yay -S qtile picom rofi nitrogen xorg-server-xephyr lxappearance-gtk3 megasync python-psutil brave-browser alacritty bat playerctl pulseaudio-ctl dunst
+git clone git@github.com:DaniDiazTech/Qtile-Config.git ~/.config/qtile
+
